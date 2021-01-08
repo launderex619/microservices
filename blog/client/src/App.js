93eff6b08885cc28a22dd2698d3e6f3e7cc6632a
ccarvajal3 <carlos.carvajal@dxc.com>
2021-01-07 22:12:53 -0600

    microservices commit video #40

--- blog/client/src/App.js
+++ blog/client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList'
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-    return (
-        <div className="container">
-            <h1>Create Post</h1>
-            <PostCreate />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Create Post</h1>
+      <PostCreate />
+      <hr />
+      <h1>Posts</h1>
+      <PostList />
+    </div>
+  );
 };
