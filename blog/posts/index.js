c5251af8d1dec91b3f578525643ccf05b1fda7a0
ccarvajal3 <carlos.carvajal@dxc.com>
2021-02-04 12:19:58 -0600

    End of blog

--- blog/posts/index.js
+++ blog/posts/index.js
@@ -14,14 +14,14 @@ app.get('/posts', (req, res, next) => {
     res.send(posts);
 });
 
-app.post('/posts', async (req, res, next) => {
+app.post('/posts/create', async (req, res, next) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
     posts[id] = {
         id, title
     };
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id, title
@@ -37,6 +37,6 @@ app.post('/events', async (req, res) => {
 });
 
 app.listen(4000, () => {
-    console.log('v1.0.1');
+    console.log('v1.2');
     console.log('Listening on 4000');
 });
